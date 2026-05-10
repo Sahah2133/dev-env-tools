2c513ac9d200551b08c6f3c1c31cbfe839769760
Sahah2133 <127717881+Sahah2133@users.noreply.github.com>
2026-05-10 20:55:22 +0330

    Delete .devcontainer/startup.sh

--- .devcontainer/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "════════════════════════════════════════════"
-echo "     Xray + Fake Speedtest Started"
-echo "════════════════════════════════════════════"
-
-nginx -g 'daemon on;'
-
-exec /usr/local/bin/xray -c /etc/config.json
